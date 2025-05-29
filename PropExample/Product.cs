b70fa9018cb1c9bc7baf8ab76478cedf416d916b
Kim Riyeon <kry59965@gmail.com>
2025-05-29 09:05:03 +0900

    생성자 - 팩토리 메서드 패턴(private 생성자 사용하는 경우)

---
+++ PropExample/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    internal class Product
+    {
+        public string name;
+        public int price;
+        static Product product;
+
+        private Product(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public static Product GetInstance(string name, int price)
+        {   // 싱글톤 패턴
+            if(product == null)
+            {
+                product = new Product(name, price);
+            }
+            return product;
+        }
+    }
+}
--- PropExample/Program.cs
+++ PropExample/Program.cs
@@ -14,10 +14,17 @@ namespace PropExample
         {
             // 클래스 변수 호출
             Program.classVar = -1;
-            
+
             // 인스턴스 변수처럼 클래스 변수 사용 불가에 주의(자바에서는 가능)
             //Program p = new Program();
             //p.classVar = -3;
+
+            // 생성자
+            // 기본(기정) 생성자 - 생성자를 하나라도 정의하면 기본 생성자를 따로 작성해줘야 함.
+            //Product p = new Product();
+
+            // 팩토리 메서드 패턴 (생성자에 private를 사용하는 경우)
+            Product p = Product.GetInstance("아아", 1500);
 
             // Property 실습
             Box box1 = new Box(100, 200);
