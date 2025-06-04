bf7880a3d3e20975f86bef6ddf0d9612cb86e03e
Kim Riyeon <kry59965@gmail.com>
2025-06-04 14:06:51 +0900

    참조 복사 예제2

--- PropExample/Program.cs
+++ PropExample/Program.cs
@@ -62,6 +62,13 @@ namespace PropExample
             test.value = 5;
             Change(test);   // 레퍼런스를 넘김에 주의
             Console.WriteLine(test.value);
+
+            // 참조 복사 예제2
+            Test testA = new Test();
+            Test testB = testA;
+            testA.value = 10;
+            testB.value = 20;
+            Console.WriteLine(testA.value);
         }
 
         class Test
